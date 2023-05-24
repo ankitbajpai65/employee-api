2919ad7b26ccf2e165121904a15cdcf601953a19
Ankit Bajpai <ankit65bajpai@gmail.com>
2023-05-24 18:24:52 +0530

    Modified app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const data = require("./data.json");
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({ data: data });
